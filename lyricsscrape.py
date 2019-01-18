0dfa35026a8ac830578bd2ffef99287d87ffd05e
milosarsik <milosarsic14@gmail.com>
2019-01-18 17:18:08 -0500

    Imported LyricsGenius Wrapper

--- lyricsscrape.py
+++ lyricsscrape.py
@@ -1,4 +1,11 @@
 from bs4 import BeautifulSoup
-import requests
+import lyricsgenius
 
-print("Welcome to Lyrics Scrape")
+base_url = 'https://api.genius.com/'
+
+genius = lyricsgenius.Genius("W35CW_FDSRpwxHZ8oh27GGtG3dAEt66EAcNG6S0zjRS26YZkueVELFwbulTLiEWl")
+
+artist = genius.search_artist("Halsey", max_songs=10)
+song = genius.search_song("Without Me", artist.name)
+print(song.lyrics)
+
